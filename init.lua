53d8f9f0df94be47c841776ba54c28819055ac61
Garrett R Low <usernamegl@live.com>
2024-12-05 23:24:10 -0600

    Add autocmd to remove trailing whitespace on save

--- init.lua
+++ init.lua
@@ -1,4 +1,17 @@
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+  pattern = { '*' },
+  callback = function()
+    local save_cursor = vim.fn.getpos '.'
+    pcall(function()
+      vim.cmd [[%s/\s\+$//e]]
+    end)
+    vim.fn.setpos('.', save_cursor)
+  end,
+})
+
 --[[
+--
+--
 
 =====================================================================
 ==================== READ THIS BEFORE CONTINUING ====================
